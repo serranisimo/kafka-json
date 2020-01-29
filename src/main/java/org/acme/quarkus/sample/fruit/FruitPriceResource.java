e5ed0522486e81ea5890f3b5f9c3eba52a0a1c64
ase <alvaro.serrano@qualityminds.de>
2020-01-29 15:29:46 +0100

    Test with fixed Stream

--- src/main/java/org/acme/quarkus/sample/fruit/FruitPriceResource.java
+++ src/main/java/org/acme/quarkus/sample/fruit/FruitPriceResource.java
@@ -1,6 +1,7 @@
 package org.acme.quarkus.sample.fruit;
 
 import io.smallrye.reactive.messaging.annotations.Channel;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
 import org.jboss.resteasy.annotations.SseElementType;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
@@ -13,22 +14,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 @Path("/fruits")
 public class FruitPriceResource {
 
     Logger LOG = LoggerFactory.getLogger(FruitPriceResource.class);
 
-    @Inject
-    @Channel("fruit-out")
-    Publisher<Fruit> fruits;
-
     @GET
     @Path("/stream")
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @SseElementType(MediaType.APPLICATION_JSON)
     public Publisher<Fruit> stream() {
-        LOG.info("JAX-RS Reactive resource - Publisher :{}", fruits);
-        return fruits;
+        ArrayList<Fruit> list = new ArrayList<Fruit>();
+        IntStream.range(0, 100).forEach(
+                nbr -> list.add(new Fruit("Fruit".concat(String.valueOf(nbr)), nbr))
+        );
+        return ReactiveStreams.fromIterable(list).buildRs();
     }
 }
